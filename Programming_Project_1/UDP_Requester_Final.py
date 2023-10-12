06621b3ac6707050e3760fa8f453f5c58cd57b04
Sahil Naphade <naphadesahil@gmail.com>
2023-10-11 22:50:20 -0500

    Adding try-catch for all socket openings

--- Programming_Project_1/UDP_Requester_Final.py
+++ Programming_Project_1/UDP_Requester_Final.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import os
 import time
 import datetime
 import math
@@ -7,85 +8,95 @@ import argparse
 
 def send_request(Sender_IP, sender_port, filename):
 	print("Sending request to {} on port {}".format(Sender_IP, sender_port))
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	seq_no = socket.htonl(0)
-	request_header = b'R' + struct.pack('II', seq_no, 0)
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		seq_no = socket.htonl(0)
+		request_header = b'R' + struct.pack('II', seq_no, 0)
 
-	sock.sendto(request_header + bytes(filename, 'utf-8'), (Sender_IP,sender_port))
-	print("Request sent \n")
-	sock.close()
+		sock.sendto(request_header + bytes(filename, 'utf-8'), (Sender_IP,sender_port))
+		print("Request sent \n")
+		sock.close()
+	except Exception as ex:
+		raise ex
 
 
 def receive_data(UDP_IP, UDP_PORT, filename):
-	sock = socket.socket(socket.AF_INET, # Internet
-						socket.SOCK_DGRAM) # UDP
-	# print("Requster waiting on IP {} @ port {}".format(UDP_IP, UDP_PORT))
-	sock.bind(('0.0.0.0', UDP_PORT))
-	start_time = None
-	count = 0
-	length_of_payload = 0
-	while True:
-		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+	try:
+		sock = socket.socket(socket.AF_INET, # Internet
+							socket.SOCK_DGRAM) # UDP
+		# print("Requster waiting on IP {} @ port {}".format(UDP_IP, UDP_PORT))
+		sock.bind(('0.0.0.0', UDP_PORT))
+		start_time = None
+		count = 0
+		length_of_payload = 0
+		while True:
+			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 
-		packet_type = data[0:1].decode('utf-8')
-		header = data[1:9]
-		header = struct.unpack('II',header)
-		sequence_number_network = header[0]
-		packet_length = header[1]
-		sequence_number = socket.ntohl(sequence_number_network)
-		count = count + 1
-		length_of_payload = length_of_payload + packet_length
+			packet_type = data[0:1].decode('utf-8')
+			header = data[1:9]
+			header = struct.unpack('II',header)
+			sequence_number_network = header[0]
+			packet_length = header[1]
+			sequence_number = socket.ntohl(sequence_number_network)
+			count = count + 1
+			length_of_payload = length_of_payload + packet_length
 
-		if start_time is None and packet_length > 0:
-			start_time = time.time()
-		payload = data[9:].decode('utf-8')
-		#payload = str(data[9:])
-		print(f"Packet Type:    {packet_type}")
-		print(f"Recv Time:      {str(datetime.datetime.now())}")
-		print(f"Sender Addr:    {addr[0]}:{addr[1]}")
-		print(f"Seq No:         {sequence_number}")
-		print(f"Length:         {packet_length}")
-		print(f"Payload:        {data[9:(9+4)].decode('utf-8')}")
-		if packet_type == 'D':
-			with open(filename, "a") as copied_file:
-				copied_file.write(payload)
-			print("\n")
+			if start_time is None and packet_length > 0:
+				start_time = time.time()
+			payload = data[9:].decode('utf-8')
+			#payload = str(data[9:])
+			print(f"Packet Type:    {packet_type}")
+			print(f"Recv Time:      {str(datetime.datetime.now())}")
+			print(f"Sender Addr:    {addr[0]}:{addr[1]}")
+			print(f"Seq No:         {sequence_number}")
+			print(f"Length:         {packet_length}")
+			print(f"Payload:        {data[9:(9+4)].decode('utf-8')}")
+			if packet_type == 'D':
+				with open(filename, "a") as copied_file:
+					copied_file.write(payload)
+				print("\n")
 
-		if packet_type == 'E':
-			end_time = time.time()
-			if start_time is None:
-				# case where the file does not exist on the sender
-				start_time = end_time
-			duration = end_time - start_time
-			break
+			if packet_type == 'E':
+				end_time = time.time()
+				if start_time is None:
+					# case where the file does not exist on the sender
+					start_time = end_time
+				duration = end_time - start_time
+				break
 
-	print("\n\n")
-	print("="*60)
-	print("Summary of Sender")
-	print(f"Sender addr:                {addr[0]}:{addr[1]}")
-	print(f"Total Data Packets:         {count - 1}")
-	print(f"Total Data Bytes:           {length_of_payload}")
-	print(f"Average packets/second:     {math.ceil((count - 1) / float(duration)) if duration != 0 else 1}")
-	print(f"Total Duration of the test: {round(duration*1000, 2)} ms")
-	print("="*60)
-	print("\n\n")
-	sock.close()
+		print("\n\n")
+		print("="*60)
+		print("Summary of Sender")
+		print(f"Sender addr:                {addr[0]}:{addr[1]}")
+		print(f"Total Data Packets:         {count - 1}")
+		print(f"Total Data Bytes:           {length_of_payload}")
+		print(f"Average packets/second:     {math.ceil((count - 1) / float(duration)) if duration != 0 else 1}")
+		print(f"Total Duration of the test: {round(duration*1000, 2)} ms")
+		print("="*60)
+		print("\n\n")
+		sock.close()
+	except Exception as ex:
+		raise ex
 
 
 def main(waiting_port, file_to_request):
 	tracker_data = []
-	with open("tracker.txt", "r") as tracker_file:
-		trackings = tracker_file.readlines()
-		for each_line in trackings:
-			filename, chunk_id, hostname, req_port = each_line.split(" ")
-			# Filter the tracker file based on the filename requested
-			if filename == file_to_request:
-				tracker_data.append((filename, int(chunk_id), hostname, int(req_port)))
-	tracker_data.sort(key=lambda x: (x[0], x[1]))
-	for filtered_host_info in tracker_data:
-		# print(f"Requesting file: {filtered_host_info[0]} Chunk ID: {filtered_host_info[1]} from Host {filtered_host_info[2]} {(socket.gethostbyname(filtered_host_info[2]))} @ port {filtered_host_info[3]}")
-		send_request(socket.gethostbyname(filtered_host_info[2]), filtered_host_info[3], file_to_request)
-		receive_data(socket.gethostbyname(socket.gethostname()), waiting_port, file_to_request)
+	if os.path.exists("tracker.txt"):
+		with open("tracker.txt", "r") as tracker_file:
+			trackings = tracker_file.readlines()
+			for each_line in trackings:
+				filename, chunk_id, hostname, req_port = each_line.split(" ")
+				# Filter the tracker file based on the filename requested
+				if filename == file_to_request:
+					tracker_data.append((filename, int(chunk_id), hostname, int(req_port)))
+		tracker_data.sort(key=lambda x: (x[0], x[1]))
+		for filtered_host_info in tracker_data:
+			# print(f"Requesting file: {filtered_host_info[0]} Chunk ID: {filtered_host_info[1]} from Host {filtered_host_info[2]} {(socket.gethostbyname(filtered_host_info[2]))} @ port {filtered_host_info[3]}")
+			send_request(socket.gethostbyname(filtered_host_info[2]), filtered_host_info[3], file_to_request)
+			receive_data(socket.gethostbyname(socket.gethostname()), waiting_port, file_to_request)
+	else:
+		raise Exception("Tracker file does not exist.")
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Receive chunks of a file from different senders")
--- Programming_Project_1/UDP_Sender_Final.py
+++ Programming_Project_1/UDP_Sender_Final.py
@@ -6,73 +6,81 @@ import argparse
 
 """Receives request from the Requester for the filename (from which data is to be sent)"""
 def receive_request(UDP_IP, UDP_PORT):
-	sock = socket.socket(socket.AF_INET, # Internet
-						socket.SOCK_DGRAM) # UDP
+	try:
+		sock = socket.socket(socket.AF_INET, # Internet
+							socket.SOCK_DGRAM) # UDP
 
-	# print("Sender waiting on IP address {} @ port {}".format(UDP_IP, UDP_PORT))
-	sock.bind(('0.0.0.0', UDP_PORT))
-	# print("Waiting on IP {} at port {}".format(UDP_IP, UDP_PORT))
+		# print("Sender waiting on IP address {} @ port {}".format(UDP_IP, UDP_PORT))
+		sock.bind(('0.0.0.0', UDP_PORT))
+		# print("Waiting on IP {} at port {}".format(UDP_IP, UDP_PORT))
 
-	while True:
-		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-		packet_type = data[0:1].decode('utf-8')
-		header = data[1:9]
-		header = struct.unpack('II', header)
-		sequence_number_network = header[0]
-		sequence_number = socket.ntohl(sequence_number_network)
-		#packet_length = struct.unpack('!I', data[5:9])[0]
-		packet_length = header[1]
+		while True:
+			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+			packet_type = data[0:1].decode('utf-8')
+			header = data[1:9]
+			header = struct.unpack('II', header)
+			sequence_number_network = header[0]
+			sequence_number = socket.ntohl(sequence_number_network)
+			#packet_length = struct.unpack('!I', data[5:9])[0]
+			packet_length = header[1]
 
-		print("Packet type:     %s"% packet_type)
-		print("Sequence no.:    %d" % sequence_number)
-		filename = data[9:].decode('utf-8')
-		print("Payload:         %s "% filename)
-		print("\n")
+			print("Packet type:     %s"% packet_type)
+			print("Sequence no.:    %d" % sequence_number)
+			filename = data[9:].decode('utf-8')
+			print("Payload:         %s "% filename)
+			print("\n")
 
-		sock.close()
-		return packet_type, filename, addr
+			sock.close()
+			return packet_type, filename, addr
+	except Exception as ex:
+		raise ex
+
 
 def create_header(packet_type, sequence_number, payload_length):
 	sequence_number_network = socket.htonl(sequence_number)
 	header = str(packet_type).encode("utf-8") + struct.pack('II', sequence_number_network, payload_length)
 	return header
 
+
 def send_packets(packet_type, requester_addr, requestor_wait_port, sequence_number, payload_length, rate, message):
-	sock = socket.socket(socket.AF_INET, # Internet
-					  socket.SOCK_DGRAM) # UDP
-	# sock.bind((requester_addr, 5000))
-	if packet_type == "D" and message != "":
-		chunks = [message[i:i + payload_length] for i in range(0, len(message), payload_length)]
+	try:
+		sock = socket.socket(socket.AF_INET, # Internet
+						socket.SOCK_DGRAM) # UDP
+		# sock.bind((requester_addr, 5000))
+		if packet_type == "D" and message != "":
+			chunks = [message[i:i + payload_length] for i in range(0, len(message), payload_length)]
 
-		for j in chunks:
-			header = create_header(packet_type, sequence_number, len(j))
-			sock.sendto((header + j.encode("utf-8")), (requester_addr, requestor_wait_port))
-			current_time = time.time()
-			milliseconds = int((current_time - int(current_time)) * 1000)
+			for j in chunks:
+				header = create_header(packet_type, sequence_number, len(j))
+				sock.sendto((header + j.encode("utf-8")), (requester_addr, requestor_wait_port))
+				current_time = time.time()
+				milliseconds = int((current_time - int(current_time)) * 1000)
 
-			print("DATA Packet")
-			print(f"Send Time:          {time.strftime('%Y-%m-%d %H:%M:%S')}.{milliseconds:03d}")
-			print(f"Requester Addr:     {requester_addr}:{requestor_wait_port}")
-			print(f"Seq No:             {sequence_number}")
-			print(f"Length:             {len(j)}")
-			print(f"Payload:            {j[9:9+4]}")
-			print("\n")
+				print("DATA Packet")
+				print(f"Send Time:          {time.strftime('%Y-%m-%d %H:%M:%S')}.{milliseconds:03d}")
+				print(f"Requester Addr:     {requester_addr}:{requestor_wait_port}")
+				print(f"Seq No:             {sequence_number}")
+				print(f"Length:             {len(j)}")
+				print(f"Payload:            {j[9:9+4]}")
+				print("\n")
 
-			sequence_number = sequence_number + len(j)
-			time.sleep(1/rate)
+				sequence_number = sequence_number + len(j)
+				time.sleep(1/rate)
 
-	end_header = create_header('E', sequence_number, 0)
-	sock.sendto(end_header + str(0).encode("utf-8"), (requester_addr, requestor_wait_port))
+		end_header = create_header('E', sequence_number, 0)
+		sock.sendto(end_header + str(0).encode("utf-8"), (requester_addr, requestor_wait_port))
 
-	current_time = time.time()
-	milliseconds = int((current_time - int(current_time)) * 1000)
-	print("END Packet")
-	print(f"Send Time:          {time.strftime('%Y-%m-%d %H:%M:%S')}.{milliseconds:03d}")
-	print(f"Requester Addr:     {requester_addr}:{requestor_wait_port}")
-	print(f"Seq No:             {sequence_number}")
-	print(f"Length:             {0}")
-	print(f"Payload:            {0}")
-	print("\n")
+		current_time = time.time()
+		milliseconds = int((current_time - int(current_time)) * 1000)
+		print("END Packet")
+		print(f"Send Time:          {time.strftime('%Y-%m-%d %H:%M:%S')}.{milliseconds:03d}")
+		print(f"Requester Addr:     {requester_addr}:{requestor_wait_port}")
+		print(f"Seq No:             {sequence_number}")
+		print(f"Length:             {0}")
+		print(f"Payload:            {0}")
+		print("\n")
+	except Exception as ex:
+		raise ex
 
 
 def main(sender_wait_port, requestor_port, packet_rate, start_seq_no, payload_length):
@@ -86,8 +94,9 @@ def main(sender_wait_port, requestor_port, packet_rate, start_seq_no, payload_le
 				# Client address is combination of IP Address and port
 				send_packets('D', client_address[0], requestor_port, start_seq_no, payload_length, packet_rate, str(message))
 		else:
-			print("File with name {} does not exist. Ending the connection.".format(filename))
+			print("File with name {} does not exist. Ending the connection.\n".format(filename))
 			send_packets('E', client_address[0], requestor_port, start_seq_no, payload_length, packet_rate, "")
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Sends chunks of a file to the requester")
